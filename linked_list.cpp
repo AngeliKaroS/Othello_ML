caebf0c8fa58a705686f8214031339941dcf9444
AngeliKaroS <75824226+AngeliKaroS@users.noreply.github.com>
2022-08-31 20:39:31 +0900

    working linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -23,4 +23,32 @@ LinkedList::LinkedList()
 int
 LinkedList::append(Node* node)
 {
+    tail->next = node;
+    tail = node;
+
+    return 0;
+}
+
+int
+LinkedList::delete(int index)
+{
+    Node* before;
+    current = head;
+    int idx = 0
+
+    while (current != NULL)
+    {
+        if (idx == index)
+        {
+            before->next = current->next
+            delete(current);
+            return 0;
+        }
+
+        idx++;
+    }
+
+    printf("index %d is not found", index);
+
+    return -1;
 }
